402e86626ccbfc65d9dacb58394c1afd37ea4748
Alejandro <aled98.ad@gmail.com>
2020-01-19 15:02:58 -0600

    beginning search modal

---
+++ src/components/SearchModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SearchModal extends Component {
+    
+    render() {
+        return <div></div>;
+    }
+    
+}
+
+export default SearchModal;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,8 +4,24 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import Axios from "axios";
 
 class TopBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.searchInput = React.createRef();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        let title = this.searchInput.current.value;
+        title = title.replace(/\s/g, "+");
+
+        Axios.get("http://www.omdbapi.com/?apikey=404f60e&t=" + title).then(res => {
+            console.log(res.data);
+        });
+    }
 
     render() {
         const topBarStyle = {
@@ -23,8 +39,8 @@ class TopBar extends Component {
                 <Nav className="mr-auto">
                 </Nav>
                 <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-secondary">Search</Button>
+                    <FormControl ref={this.searchInput} type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-light" onClick={this.handleClick} >Search</Button>
                 </Form>
             </NavBar.Collapse>
         </NavBar>
